4b8e029d9e33f751844b30e71c91880f39eb8d13
Artemy Egorov <artegoser@gmail.com>
2026-06-28 20:53:19 +0300

    feat(server): add request logging

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::{
 };
 
 use tower_http::services::ServeDir;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 mod config;
 mod drova;
@@ -19,27 +21,54 @@ use state::AppState;
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let config = Arc::new(Config::from_env());
+    let configured_host = config.host.clone();
+    let configured_port = config.port;
     let addr = SocketAddr::from((
-        config
-            .host
-            .parse::<IpAddr>()
-            .unwrap_or_else(|_| [0, 0, 0, 0].into()),
+        config.host.parse::<IpAddr>().unwrap_or_else(|error| {
+            tracing::warn!(
+                host = %config.host,
+                %error,
+                "invalid HOST, falling back to 0.0.0.0"
+            );
+            [0, 0, 0, 0].into()
+        }),
         config.port,
     ));
     let static_dir = std::env::var("TXTDOT_STATIC_DIR")
         .unwrap_or_else(|_| env!("TXTDOT_BUILD_STATIC_DIR").to_string());
+    tracing::info!(
+        host = %configured_host,
+        port = configured_port,
+        bind = %addr,
+        static_dir = %static_dir,
+        "starting txtdot v2 server"
+    );
 
     let app = routes::router(AppState {
         config,
         routes: Arc::new(routes::app_routes()),
     })
-    .nest_service("/static", ServeDir::new(static_dir));
+    .nest_service("/static", ServeDir::new(static_dir))
+    .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind(addr)
         .await
         .expect("bind txtdot v2 server");
+    let local_addr = listener
+        .local_addr()
+        .expect("read txtdot v2 server listener address");
+    tracing::info!(
+        ip = %local_addr.ip(),
+        port = local_addr.port(),
+        addr = %local_addr,
+        "txtdot v2 server listening"
+    );
 
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown_signal())
